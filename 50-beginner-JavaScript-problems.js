0bbb1d7723a195fbf4c90e9aa77d5b25c3225c78
big21josh <big21josh@gmail.com>
2026-06-25 10:48:18 +0200

    update

--- 50-beginner-JavaScript-problems.js
+++ 50-beginner-JavaScript-problems.js
@@ -7,3 +7,9 @@ console.log(name);
 // 2. Store your age in a variable and print it.
 const age = 20;
 console.log(age);
+
+// 3. Create variables for length and width and calculate the area of a rectangle.
+const length = 10;
+const width = 5;
+const area = length * width;
+console.log(area);
