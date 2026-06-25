fe5a33cab3a06ecb63c48fa92444521aff7dd22d
big21josh <big21josh@gmail.com>
2026-06-25 10:47:47 +0200

    update

---
+++ 50-beginner-JavaScript-problems.js
@@ -0,0 +1,9 @@
+// VARIABLES AND DATA TYPES
+
+// 1. Declare a variable called name and store your name in it.
+const name = "Josh";
+console.log(name);
+
+// 2. Store your age in a variable and print it.
+const age = 20;
+console.log(age);
